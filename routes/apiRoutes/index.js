45320fd1180fea79838f129be6aeb3f6928df3ba
nelsondcds <pipoca012008@yahoo.com>
2021-09-02 02:30:36 -0400

    finished note taker, having strange issues with deletion, going to test on heroku

---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,28 @@
+const { v4: uuidv4 } = require('uuid');
+const { deleteById, createNewNote, validateNote } = require('../../lib/notes');
+const { notes } = require('../../db/db');
+const router = require('express').Router();
+
+
+router.get('/notes', (req, res) => {
+    let results = notes;
+    res.json(results);
+});
+
+router.delete('/notes/:id', (req, res) => {
+	const result = deleteById(req.params.id, notes);
+	res.json(result);
+});
+
+router.post('/notes', (req, res) => {
+	req.body.id = uuidv4();
+
+	if (!validateNote(req.body)) {
+	  	res.status(400).send('The note is not properly formatted.');
+	} else {
+	    const note = createNewNote(req.body, notes);
+	    res.json(note);
+	}
+});
+
+module.exports = router;
